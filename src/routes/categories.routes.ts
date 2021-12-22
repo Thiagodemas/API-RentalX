fcbebac6a48dddeb006d6509a5daabc01400967c
thiagodemas7 <thiago.demas@ignicaodigital.com.br>
2021-12-22 12:02:42 -0300

    Adiciona ICategoriesRepository e PostgresCategoriesRepository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoryRepository = new CategoriesRepository();
@@ -8,13 +9,9 @@ const categoryRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const categoryAlreadyExists = categoryRepository.findByName(name);
+    const createCategoryService = new CreateCategoryService(categoryRepository);
 
-    if (categoryAlreadyExists) {
-        return response.status(400).json({ error: "Category already exists!" });
-    }
-
-    categoryRepository.create({ name, description });
+    createCategoryService.execute({ name, description });
 
     return response.status(201).send();
 });
